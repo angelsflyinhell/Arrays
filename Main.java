3828a5c1376809c20038a7542a9b3e91d3f2faee
yuki <angelsflyinhell@namespace.media>
2022-08-22 13:44:42 +0200

    j@@@@

--- Main.java
+++ Main.java
@@ -86,7 +86,7 @@ public class Main {
         for(int i = 0; i < a.length; i++) {
             for(int j = i + 1; j < a.length; j++) {
                 if(a[i] > a[j])
-                    a = swap(a,j, i);
+                    swap(a,j, i);
             }
         }
         printArray(a);
